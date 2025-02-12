809022fbc2634d8241511184114add3457454987
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-02-12 14:38:05 +0100

    starter

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import AuthContextProvider from './contexts/AuthContext.jsx';
 import ToasterProvider from './contexts/ToasterContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ToasterProvider>
-        <AuthContextProvider>
-          <App />
-        </AuthContextProvider>
+        <App />
       </ToasterProvider>
     </BrowserRouter>
   </StrictMode>
